8a500b41048dc9884dcbb3b55a6b21ff4abfb2cb
Vadim Adelbaev <greates@yandex.ru>
2018-07-14 07:46:26 -0400

    add resource for firewall

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@ provider "google" {
   project = "infra-208108"
   region = "europe-west1"
 }
+
 resource "google_compute_instance" "app" {
   name = "reddit-app"
   machine_type = "g1-small"
@@ -19,5 +20,16 @@ resource "google_compute_instance" "app" {
   metadata {
     ssh-keys = "greates:${file("~/.ssh/gcp.pub")}"
   }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports = ["9292"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["reddit-app"]
 }
 
