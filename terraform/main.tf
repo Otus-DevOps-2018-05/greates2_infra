489d492351c8d04e2f05d6795e46ad71aab5d8fc
Vadim Adelbaev <greates@yandex.ru>
2018-07-14 07:48:46 -0400

    add tags on main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,7 @@ resource "google_compute_instance" "app" {
   name = "reddit-app"
   machine_type = "g1-small"
   zone = "europe-west1-b"
+  tags = ["reddit-app"]
   boot_disk {
     initialize_params {
       image = "reddit-base-1531474243"
