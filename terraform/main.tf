5a267a5b6bf21de6fe14cca3c5bc2aed14d75781
Vadim Adelbaev <greates@yandex.ru>
2018-07-21 06:36:00 -0400

    add source app-ip and firewall-ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,3 +54,19 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name    = "default-allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
+}
