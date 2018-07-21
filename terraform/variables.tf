5ed8b3839709eaa7d250f5834fe5369071f5891a
Vadim Adelbaev <greates@yandex.ru>
2018-07-21 09:36:40 -0400

    fix variables.tf , add zone section

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,6 +7,11 @@ variable region {
   default     = "europe-west1"
 }
 
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+
 variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
