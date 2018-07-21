05c3e518ce58775c261982e333634bb396a82f0f
Vadim Adelbaev <greates@yandex.ru>
2018-07-21 06:47:33 -0400

    add nat_ip in access_config (main.tf)

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,8 +17,11 @@ resource "google_compute_instance" "app" {
   }
 
   network_interface {
-    network       = "default"
-    access_config = {}
+    network = "default"
+
+    access_config = {
+      nat_ip = "${google_compute_address.app_ip.address}"
+    }
   }
 
   metadata {
